1ff947970e93d5f916989c7ea546b552c7a47681
tteletubbie <ttb.snow2@gmail.com>
2026-08-07 10:30:55 -0600

    Added code to about functions

--- about/about.py
+++ about/about.py
@@ -11,15 +11,43 @@ from ui import visuals
 console = Console()
 
 def show_staff():
-    #Your code goes here, delete it when finished
-    console.input()
+    while True:
+        console.clear()
+        console.print(Align.center(Panel(Align.center("[bold green][/bold green]"), title="[bold #00FFB3]BookWorm Admins [/bold #00FFB3]", border_style="#00FFB3",width=50)))
+
+        console.print("[bold]Luis [green]Gael [/green]Gonzalez Torres\n")
+        console.print("[bold][red]Carlos[/red] Silva Cortes\n")
+        console.print("[bold][magenta]Lesly [/magenta] Adilene Terrazo Rodriguez\n")
+        console.print("[bold]Maria [purple]Fernanda[/purple] Obregon Ramirez")
+
+        console.input()
+        break
 
 
 def show_rules():
-    #Your code goes here, delete it when finished
-    console.input()
+    while True:
+        console.clear()
+        console.print(Align.center(Panel(Align.center("[bold green][/bold green]"), title="[bold #00FFB3]BookWorm Rules [/bold #00FFB3]", border_style="#00FFB3",width=50)))
+
+        console.print("[bold][green]1.[/green] Be respectful to others.\n")
+        console.print("[bold][green]2.[/green] No spamming or advertising.\n")
+        console.print("[bold][green]3.[/green] Use appropriate language.\n")
+        console.print("[bold][green]4.[/green] Follow the community guidelines.\n")
+        console.print("[bold][green]5.[/green] Report any suspicious activity.\n")
+
+        console.input()
+        break
 
 
 def show_made_by():
-    #Your code goes here, delete it when finished
-    console.input()
+    while True:
+        console.clear()
+        console.print(Align.center(Panel(Align.center("[bold green][/bold green]"), title="[bold #00FFB3]Made By [/bold #00FFB3]", border_style="#00FFB3",width=50)))
+
+        console.print("[bold]Luis [green]Gael [/green]Gonzalez Torres\n")
+        console.print("[bold][red]Carlos[/red] Silva Cortes\n")
+        console.print("[bold][magenta]Lesly [/magenta] Adilene Terrazo Rodriguez\n")
+        console.print("[bold]Maria [purple]Fernanda[/purple] Obregon Ramirez")
+
+        console.input()
+        break
